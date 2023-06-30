c369328122a02e76739651339cecf9efc07dd81b
Cheezeco <matas.g.terrait@gmail.com>
2023-06-30 16:24:47 +0300

    Add ForEach extension to IEnumerable

---
+++ NitroBrew/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NitroBrew.Extensions
+{
+    internal static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> values, Action<T> action)
+        {
+            foreach (var item in values)
+            {
+                action.Invoke(item);
+            }
+        }
+    }
+}
